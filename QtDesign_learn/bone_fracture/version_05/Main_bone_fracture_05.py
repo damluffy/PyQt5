131847f514fafb8bbe208c754fadaf3e9d055d0e
damluffy <37398043+damluffy@users.noreply.github.com>
2019-01-28 10:28:13 +0800

    Add files via upload

---
+++ QtDesign_learn/bone_fracture/version_05/Main_bone_fracture_05.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 10 15:02:06 2019
+
+@author: ai-pc
+"""
+
+from Link_bone_fracture_05 import MyMainWindow
+
+class MyMain(MyMainWindow):
+    def __init__(self,parent=None):
+        super(MyMain, self).__init__(parent)
+
+        
+
+import sys
+from PyQt5.QtWidgets import QApplication
+
+if __name__=='__main__':
+    app=QApplication(sys.argv)
+    myWin=MyMainWindow()
+    myWin.show()
+    sys.exit(app.exec_())
